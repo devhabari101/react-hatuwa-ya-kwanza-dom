00a1d35a91832f7877d1ff6341bf7fcab0b67003
devhabari101 <125182471+devhabari101@users.noreply.github.com>
2024-02-11 19:37:30 +0300

    Update data.js

--- src/components/data.js
+++ src/components/data.js
@@ -1,42 +1,82 @@
 export const planets = [
   {
-    id: 0,
-    jina: "Dunia",
-    alama: "🜨 and ♁",
-    chimbuko: "hakuna taarifa",
-    mgunduzi: "",
-    imageId: "vhSz50AaFAs",
-  },
-  {
-    id: 1,
     jina: "Saturn",
     alama: "♄",
-    chimbuko: "Saturn",
-    mgunduzi: "",
-    imageId: "2W-QWAC0mzI",
+    maelezo:
+      "Surturn ni sayari ya sita kutoka kwenye jua na ya pili kwa ukubwa kwenye mfumo wa sayari, imezungukwa na ringi",
+    url: "https://source.unsplash.com/2W-QWAC0mzI/200x200",
+    alt: "Saturn.",
   },
   {
-    id: 2,
+    jina: "Dunia",
+    alama: "🜨 and ♁",
+    maelezo:
+      "Dunia ni sayari yatatu kutoka kwenye jua na ndiyo sayari Pekee inayo fahamika kua na maisha. Na hii imewezekana Kwa sabubu ya kuzungukwa na maji kwa 70.8%",
+    url: "https://source.unsplash.com/vhSz50AaFAs/200x200",
+    alt: "A gigantic metallic flower sculpture with reflective mirror-like petals and strong stamens.",
+  },
+  {
     jina: "Venus",
     alama: "♀",
-    chimbuko: "Miungu ya mapenzi ya Roman",
-    mgunduzi: "",
-    imageId: "88BMUbQHGlQ",
+    maelezo:
+      'Venus ni sayari ya pili kutoka kwenye jua. Nimoja kati yazile sayari za mwamba "rock planet" au terrestrial planet hewa yake ni carbon dioxide iliyo zito na yenye joto kuliko ile ya duniani, uso wake una asili ya volcano',
+    url: "https://source.unsplash.com/88BMUbQHGlQ/200x200",
+    alt: "Venus.",
   },
   {
-    id: 3,
     jina: "uranus",
     alama: "⛢ au ♅",
-    chimbuko: "wa Latin kutoka Ūranus wa mungu wa giriki Οὐρανός Ouranos",
-    mgunduzi: "Mgunduzi William Herschel",
-    imageId: "Li41RApUAQA",
+    maelezo:
+      "Uranus ni sayari ya saba kutoka kwenye jua sayari hii kwa kiwango kikubwa imetengenenzwa na maji ammonia, na methane katika hatua muhimu ya kuganisha kwenye lugha ya astronomy tunasema 'ice' au volatiles.",
+    url: "https://source.unsplash.com/Li41RApUAQA/200x200",
+    alt: "uranus",
   },
   {
-    id: 4,
     jina: "Mercury",
     alama: "☿",
-    chimbuko: "",
-    mgunduzi: "hakuna taarifa",
-    imageId: "71W3CWeZF7A",
+    maelezo:
+      'Mercury ni sayari ya kwanza kutoka kwenye jua na ndiyo ndogo kuliko zote kwenye mfumo wa jua kwa kingereza, imeitwa baada ya mungu wa ki Roman Mercurius (Mercury), mungu wa biyashara na mawasiliano na mjumbe wa miungu. Mercury ipo kwenye kundi la sayari za mwamba "terrestrial planet" nguvu yake ya mvutano nikama ile ya Mars',
+    url: "https://source.unsplash.com/71W3CWeZF7A/200x200",
+    alt: " Mercury ni sayari ya kwanza kutoka kwenye jua.",
+  },
+  {
+    jina: "Neptune",
+    alama: "♆",
+    maelezo:
+      "Ni ya nane na ya mbali kutoka kwenye jua. Ni sayari ya nnne kwa ukubwa kwenye mfumo wa jua kwa kipenyo, ina uzito mara 17 zaidi ya sayari ya Dunia na zito kiasi kuliko ile sayari ya barafu Uranus",
+    url: "https://source.unsplash.com/m35LirqP6y8/200x200",
+    alt: "Neptune ni ya nane na ya mbali kutoka kwenye jua",
+  },
+  {
+    jina: "Mars",
+    alama: "♂",
+    maelezo:
+      'Mars ni sayari ya nne kutoka kwenye jua.Uso wa Mars ni wa manjano-mekundu umefunikwa na vumbi la  iron(III) oxide na hivyo kupatiwa jina la utani la sayari "nyekundu".Mars ni moja kati ya vitu vinavyo ngaa sana kwenye anga ya Dunia',
+    url: "https://source.unsplash.com/N3BQHYOVq5E/200x200",
+    alt: "Mars ni sayari ya nne kutoka kwenye jua.",
+  },
+  {
+    jina: "Jupiter",
+    alama: "♃",
+    maelezo:
+      'Jupiter ni sayari yatano kutoka kwenye jua na kubwa kuliko zote kwenye mfumo wa juwa ina kiwango kikubwa cha gas zaidi ya mara mbili na nusu ya sayari zote kwenye mfumo wa jua zikichanganywa, na kiasi kidogo chini ya jua. Imetengenenzwa na hydrogen "90% kwa ujazo", ikifutiwa na helium. Jupiter ni kitu cha tatu cha asili kinacho ngaa kwenye angaa ya dunia usiku baada ya Mwezi na Venus. I liitwa baada ya mungu mkuu wa dini ya zamani ya Roman',
+    url: "https://source.unsplash.com/_B7LjqNXu5Q/200x200",
+    alt: "Jupiter ni sayari yatano kutoka kwenye jua na kubwa kuliko zote kwenye mfumo wa juwa.",
+  },
+  {
+    jina: "Mwezi",
+    alama: "☾ au ☽",
+    maelezo:
+      "Mwezi ndio satelaiti pekee ya asili duniani. Inazunguka kwa umbali wa wastani wa kilomita 384,400 (238,900 mi), karibu mara 30 ya kipenyo cha Dunia. Mwezi muda wote upo upande sawa na Dunia, kwa sababu ya nguvu ya mvuto ambayo imefunga mzunguko wake kwenye sayari. Hii husababisha kua na siku 29.5 za muandamo Dunia zinazolingana na mwezi mwandamo. Nguvu ya mvutano ya Mwezi - na kwa kiasi kidogo ya Jua - ndio vichochezi vikuu vya mawimbi.",
+    url: "https://source.unsplash.com/TRXSkmJb40c/200x200",
+    alt: "Mwezi.",
+  },
+  {
+    jina: "Jua",
+    alama: "Circle with dot in the middle",
+    maelezo:
+      "Jua ni nyota iliyo katikati ya Mfumo wa Jua. Ni mpira mkubwa, moto wa plasma, umechangiwa na kupashwa joto na nishati inayozalishwa na athari za muunganisho wa nyuklia katika kiini chake. Sehemu ya nishati hii ya ndani hutolewa kutoka kwenye uso wake kama mionzi ya mwanga, urujuanimno na infrared, ikitoa nishati nyingi kwa maisha duniani.",
+    url: "https://source.unsplash.com/RySPDa0FdcQ/200x200",
+    alt: "Jua",
   },
 ];
