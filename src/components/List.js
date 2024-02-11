adaa1632d57c0f23ebf610f4215d43fecad53895
devhabari101 <125182471+devhabari101@users.noreply.github.com>
2024-02-11 11:35:36 +0300

    Update List.js

--- src/components/List.js
+++ src/components/List.js
@@ -1,12 +1,25 @@
-function List() {
+import { planets } from "./data.js";
+import { getImageUrl } from "./utils.js";
+
+export default function List() {
+  const orodhaVitu = planets.map((sayari) => (
+    <li key={sayari.id}>
+      <img src={getImageUrl(sayari)} alt={sayari.jina} />
+
+      <p>
+        Jina la sayari <b>{"" + sayari.jina}</b>
+      </p>
+      <p>alama {" " + sayari.alama + " "}</p>
+      <p>
+        imeitwa jina la <b>{sayari.chimbuko}</b>
+      </p>
+      <p>{sayari.mgunduzi}</p>
+    </li>
+  ));
   return (
-    <>
-      <img
-        class="img-fluid"
-        src="https://source.unsplash.com/cZkdauWij50/200x200"
-      />
-    </>
+    <article>
+      <h1>Elimu ya Sayari na Sayansi ya Vimondo</h1>
+      <ul>{orodhaVitu}</ul>
+    </article>
   );
 }
-
-export default List;
